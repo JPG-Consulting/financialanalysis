054f83b18cf2ba9810ddc206997a6a8397776d82
sgastia <sgastia@gamil.com>
2017-10-16 13:35:27 -0300

    Adding Entity Framework - missing some files

---
+++ Analyst_sln/Analyst.DBAccess/Contexts/AnalystContext.cs
@@ -0,0 +1,26 @@
+﻿using Analyst.Domain;
+using Analyst.Domain.Edgar.Datasets;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Analyst.DBAccess.Contexts
+{
+    internal class AnalystContext: DbContext
+    {
+
+        public AnalystContext()
+            : base("name=Analyst")
+        {
+        }
+
+        
+
+        public virtual DbSet<EdgarDataset> DataSets { get; set; }
+        
+
+    }
+}
---
+++ Analyst_sln/Analyst.DBAccess/Contexts/AnalystRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Analyst.Domain.Edgar.Datasets;
+
+namespace Analyst.DBAccess.Contexts
+{
+    public interface IAnalystRepository
+    {
+        List<EdgarDataset> GetDatasets();
+        void AddDataset(EdgarDataset ds);
+        int GetDatasetsCount();
+    }
+
+    public class AnalystRepository: IAnalystRepository
+    {
+
+        private AnalystContext _context;
+        internal AnalystContext Context
+        {
+            //TODO: Implementar inyeccion de dependencias
+            get {
+                if(_context == null)
+                    _context = new AnalystContext();
+                return _context;
+            }
+            set { }
+        }
+        public List<EdgarDataset> GetDatasets()
+        {
+            Context = new AnalystContext();
+            return Context.DataSets.ToList();
+        }
+
+        public int GetDatasetsCount()
+        {
+            return Context.DataSets.Count();
+        }
+
+        public void AddDataset(EdgarDataset ds)
+        {
+            Context.DataSets.Add(ds);
+            Context.SaveChanges();
+        }
+    }
+}
