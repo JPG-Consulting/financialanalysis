66501c4c65d7ea6db4b378858aa7e8b4b514dfd2
Sebastian <sgastia@gmail.com>
2020-04-17 02:59:46 +0200

    Added wrapper for full text searcher framework

---
+++ Analyst_sln/FinancialAnalyst.TextSearchers/Lucene.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FinancialAnalyst.TextSearchers
+{
+    /// <summary>
+    /// https://lucenenet.apache.org/
+    /// </summary>
+    public class Lucene
+    {
+    }
+}
