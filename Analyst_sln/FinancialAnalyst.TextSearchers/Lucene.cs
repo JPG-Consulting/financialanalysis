6ac7c93524df3b4a002d2de602797f83828ff2d9
Sebastian <sgastia@gmail.com>
2020-04-21 01:46:52 +0200

    Breve explicacion de framework Lucene

--- Analyst_sln/FinancialAnalyst.TextSearchers/Lucene.cs
+++ Analyst_sln/FinancialAnalyst.TextSearchers/Lucene.cs
@@ -4,6 +4,24 @@ namespace FinancialAnalyst.TextSearchers
 {
     /// <summary>
     /// https://lucenenet.apache.org/
+    /// 
+    /// Apache Lucene es una API de código abierto para recuperación de información, originalmente implementada en Java por Doug Cutting. 
+    /// Está apoyado por el Apache Software Foundation y se distribuye bajo la Apache Software License. 
+    /// Lucene tiene versiones para otros lenguajes incluyendo Delphi, Perl, C#, C++, Python, Ruby y PHP.
+    /// 
+    /// Es útil para cualquier aplicación que requiera indexado y búsqueda a texto completo.
+    /// Lucene ha sido ampliamente usado por su utilidad en la implementación de motores de búsquedas.
+    /// Por ello, a veces se confunde Lucene con un motor de búsquedas con funciones de "crawling" y análisis de documentos en HTML incorporadas.
+    /// 
+    /// El centro de la arquitectura lógica de Lucene se encuentra el concepto de Documento(Document) que contiene Campos(Fields) de texto.
+    /// Esta flexibilidad permite a Lucene ser independiente del formato del fichero. 
+    /// Textos que se encuentran en PDFs, páginas HTML, documentos de Microsoft Word, así como muchos otros pueden ser indexados mientras que se pueda extraer información de ellos.
+    /// 
+    /// Nutch es un software que, sobre la base aportada por Lucene, integra todo lo que hace falta para completar un motor de búsqueda de páginas web.
+    /// 
+    /// http://es.wikipedia.org/wiki/Lucene
+    /// 
+    /// http://lucene.apache.org/core/
     /// </summary>
     public class Lucene
     {
